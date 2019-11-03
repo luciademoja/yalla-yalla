c31557e8ea176eace25caa1d38de957e53afb925
Lucia De Mojà <luciademoja@gmail.com>
2019-11-03 20:41:17 +0100

    stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,14 +37,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer style={{ fontSize: `12px` }}>
-          <div style={{ fontSize: `12px` }}>Icons made by
-          <a href="https://www.freepik.com/" title="Freepik">Freepik </a>
-          from
-          <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com </a>
-          is licensed by
-          <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" rel="noopener noreferrer" target="_blank"> CC 3.0
-            BY</a>
-        </div>
+
         </footer>
       </div>
     </>
