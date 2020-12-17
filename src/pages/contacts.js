719d6c7091df85e8ab17048ba43fcaff94c014a1
Lucia De Mojà <luciademoja@gmail.com>
2020-12-17 17:04:06 +0100

    update contact page

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -2,25 +2,25 @@ import React from "react"
 import SquareImages from "../components/squareImages"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Card } from "antd"
 
 const Contacts = () => (
   <Layout>
     <SEO title="Kontakt yalla yalla" />
     <div className="welcome">
-      <h2>Välkommen till Yalla Yalla Meze kök och bar!</h2>
-      <h4>
-        Vi serverar de godaste Grekiska, Turkiska och Libanesiska maträtter.
-      </h4>
       <br/>
       <h4>Ring: <a className='contact' href="tel:031140084">031 14 00 84</a></h4>
-      <h4>Eller maila: <a className='contact' href="mailto:yalla_yalla@hotmail.se">yalla_yalla@hotmail.se</a></h4>
-      <h4>Vi finns på tredje Långgatan 29, 413 28 Göteborg.</h4>
-      <ul className="openings">Öppettider:
-        <li>Mån-tors 16:00 - 21:00</li>
-        <li>Fredag 16:00 - 23:00</li>
-        <li>Lördag 13:00 - 23:00</li>
-        <li>Söndag 13:00- 21:00</li>
-      </ul>
+      <h4>Maila: <a className='contact' href="mailto:yalla_yalla@hotmail.se">yalla_yalla@hotmail.se</a></h4>
+      <h4>Eller besök oss på: <a className="contact" href="https://www.google.com/maps/place/Yalla/@57.6984096,11.9449591,17z/data=!3m1!4b1!4m5!3m4!1s0x464ff5bc9165e315:0xb9b0a990bbde26cc!8m2!3d57.6984068!4d11.9471531">Tredje Långgatan 29, 413 28 Göteborg</a></h4>
+      <h2>Välkommna!</h2>
+      <Card title="Öppettider" className='foodora'>
+        <ul className="openings">
+          <li>Mån-tors 16:00 - 21:00</li>
+          <li>Fredag 16:00 - 23:00</li>
+          <li>Lördag 13:00 - 23:00</li>
+          <li>Söndag 13:00- 21:00</li>
+        </ul>
+      </Card>
       <br/>
       <SquareImages />
     </div>
