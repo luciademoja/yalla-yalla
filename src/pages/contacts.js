5cf0197444afe2a9823958401c957fdaeee8a7fe
Lucia De Mojà <luciademoja@gmail.com>
2021-06-09 14:59:21 +0200

    update opening hours

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -15,10 +15,12 @@ const Contacts = () => (
       <h2>Välkommna!</h2>
       <Card title="Öppettider" className='foodora'>
         <ul className="openings">
-          <li>Måndag - Torsdag: Stängt</li>
-          <li>Fredag 16:00 - 23:00</li>
-          <li>Lördag 13:00 - 23:00</li>
-          <li>Söndag 13:00- 21:00</li>
+          <li>Mån/Tis Stängt</li>
+          <li>Ons 16:00 - 21:00</li>
+          <li>Tor 16:00 - 21:00</li>
+          <li>Fre 16:00 - 23:00</li>
+          <li>Lör 13:00 - 23:00</li>
+          <li>Sön 13:00 - 21:00</li>
         </ul>
       </Card>
       <br/>
